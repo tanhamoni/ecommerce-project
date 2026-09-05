2849fd89e6aca33085aae8164be5d2cb07b19dcd
tanhamoni <tasniya.bd26@gmail.com>
2026-09-05 19:24:14 +0600

    active setting and policy seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,8 +23,8 @@ class DatabaseSeeder extends Seeder
 
 
 
-        // $this->call(SettingSeeder::class);
+        $this->call(SettingSeeder::class);
         
-        // $this->call(PolicySeeder::class);
+        $this->call(PolicySeeder::class);
     }
 }
