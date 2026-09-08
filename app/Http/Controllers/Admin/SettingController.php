8bd009e1858bc5c13626ff4eb10a4e6aec6a67af
tanhamoni <tasniya.bd26@gmail.com>
2026-09-08 20:18:03 +0600

    Use unsigned upload with sjdi3oza preset

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Setting;
 use App\Models\WebsitePolicy;
 use Illuminate\Http\Request;
-use Cloudinary\Configuration\Configuration;
 use Cloudinary\Api\Upload\UploadApi;
 
 class SettingController extends Controller
@@ -29,19 +28,19 @@ class SettingController extends Controller
         $websiteSettings->youtube = $request->youtube;
         $websiteSettings->instagram = $request->instagram;
 
-        // Directly pass config array into UploadApi constructor
-        $uploadApi = new UploadApi([
-            'cloud' => [
-                'cloud_name' => 'zazc3c7b',
-                'api_key'    => '239595857632991',
-                'api_secret' => '5kzAiJfZ91WpO5xw8-yULKs5SBg',
-            ]
-        ]);
+        // Unsigned Config Options
+        $options = [
+            'cloud_name'    => 'zazc3c7b',
+            'upload_preset' => 'sjdi3oza',
+        ];
+
+        $uploadApi = new UploadApi();
 
         // Logo Upload
         if ($request->hasFile('logo')) {
             $uploadedLogo = $uploadApi->upload(
-                $request->file('logo')->getRealPath()
+                $request->file('logo')->getRealPath(),
+                $options
             );
             $websiteSettings->logo = $uploadedLogo['secure_url'];
         }
@@ -49,7 +48,8 @@ class SettingController extends Controller
         // Hero Image Upload
         if ($request->hasFile('hero_image')) {
             $uploadedHero = $uploadApi->upload(
-                $request->file('hero_image')->getRealPath()
+                $request->file('hero_image')->getRealPath(),
+                $options
             );
             $websiteSettings->hero_image = $uploadedHero['secure_url'];
         }
