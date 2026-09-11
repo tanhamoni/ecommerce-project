54c18e8f62b826a12b260f1982504085c355e0c3
tanhamoni <tasniya.bd26@gmail.com>
2026-09-11 20:11:29 +0600

    Force tmp storage in api/index.php

--- public/index.php
+++ public/index.php
@@ -1,55 +1,37 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+// Force Vercel to use /tmp for all write operations
+$storagePath = '/tmp/storage';
 
-define('LARAVEL_START', microtime(true));
+$dirs = [
+    $storagePath . '/framework/views',
+    $storagePath . '/framework/cache/data',
+    $storagePath . '/framework/sessions',
+    $storagePath . '/logs',
+    '/tmp/views'
+];
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        @mkdir($dir, 0755, true);
+    }
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
+// Override Laravel Environment Paths before runtime
+putenv("APP_STORAGE_PATH={$storagePath}");
+$_ENV['APP_STORAGE_PATH'] = $storagePath;
 
-require __DIR__.'/../vendor/autoload.php';
+putenv("VIEW_COMPILED_PATH=/tmp/views");
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
+putenv("LOG_CHANNEL=stderr");
+$_ENV['LOG_CHANNEL'] = 'stderr';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+putenv("CACHE_DRIVER=array");
+$_ENV['CACHE_DRIVER'] = 'array';
 
-$kernel = $app->make(Kernel::class);
+putenv("SESSION_DRIVER=array");
+$_ENV['SESSION_DRIVER'] = 'array';
 
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+// Load Public Index
+require __DIR__ . '/../public/index.php';
